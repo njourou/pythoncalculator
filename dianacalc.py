5664b4ec65603b026da76447a5700eb3ebb85438
DAINAWAMBUI <61407935+DAINAWAMBUI@users.noreply.github.com>
2021-06-30 17:13:53 +0300

    Add files via upload

---
+++ dianacalc.py
@@ -0,0 +1,16 @@
+num1=int(input("enter the first number:"))
+num2=int(input("enter the second number:"))
+sum=num1+num2
+print(sum)
+num1=int(input("enter the first number:"))
+num2=int(input("enter the second number:"))
+mult=num1*num2
+print(mult)
+num1=int(input("enter the first number:"))
+num2=int(input("enter the second number:"))
+sub=num1-num2
+print(sub)
+num1=int(input("enter the first number:"))
+num2=int(input("enter the second number:"))
+div=num1/num2
+print(div)
